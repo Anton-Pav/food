59b7e477c48595d550ccc75a7b0eca94b4869739
Anton <ant.pavlov93@yandex.ru>
2022-09-14 21:01:12 +0300

    методы апишки

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import {Link} from "react-router-dom";
+
 function Header() {
     return <nav className='green darken-1'>
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Movies</a>
+            <a href="/" className="brand-logo">Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Repo</a></li>
+                <li>
+                    <Link to="/about">About</Link></li>
+                <li>
+                    <Link to="/contact">Contacts</Link></li>
             </ul>
         </div>
     </nav>

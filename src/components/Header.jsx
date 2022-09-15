59b4acb3ff5fb45db39d39deb3d507cdb81c3a36
Anton <ant.pavlov93@yandex.ru>
2022-09-15 15:26:14 +0300

    поиск блюд на главной странице

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import {Link} from "react-router-dom";
 function Header() {
     return <nav className='green darken-1'>
         <div className="nav-wrapper">
-            <a href="/" className="brand-logo">Shop</a>
+            <a href="/" className="brand-logo">
+                Food recipes</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li>
                     <Link to="/about">About</Link></li>

94349a165b8e955198b8f462cf58e6e1ae2be80c
Anton <ant.pavlov93@yandex.ru>
2022-09-15 14:41:25 +0300

    страница с рецептами

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
 import {getFilteredByCategory} from "../api";
 import {Preloader} from "../components/Preloader";
 import {MealList} from "../components/MealList";
@@ -7,14 +7,18 @@ import {MealList} from "../components/MealList";
 function Category() {
     const {name} = useParams()
     const [meals, setMeals] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         getFilteredByCategory(name).then(data => setMeals(data.meals))
     }, [name])
 
-    return <>
-        {!meals.length ? <Preloader/> : <MealList meals={meals}/>}
-    </>
+    return (
+        <>
+            <button className='btn' onClick={() => navigate(-1)}>Go Back</button>
+            {!meals.length ? <Preloader/> : <MealList meals={meals}/>}
+        </>
+    )
 }
 
 export {Category}
